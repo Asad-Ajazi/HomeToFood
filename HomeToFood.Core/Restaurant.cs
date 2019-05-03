e3e65269974cd51afd28526bb1599acbaeb4f217
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-05-03 19:02:27 +0100

    Created a data access class library and added an in memory list to temporarily use to display data that implements and interface.

--- HomeToFood.Core/Restaurant.cs
+++ HomeToFood.Core/Restaurant.cs
@@ -10,5 +10,7 @@ namespace HomeToFood.Core
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
     }
 }
+
---
+++ HomeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using HomeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HomeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{ Id = 1, Name = "Hot Pizza", Location = "Maryland", Cuisine = CuisineType.Italian },
+                new Restaurant{ Id = 2, Name = "Bollywoods", Location = "London", Cuisine = CuisineType.Indian },
+                new Restaurant{ Id = 3, Name = "Corazon", Location = "LA", Cuisine = CuisineType.Mexican }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
