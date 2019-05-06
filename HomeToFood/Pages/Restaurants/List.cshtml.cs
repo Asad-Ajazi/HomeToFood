28d5ac6920b04e7481eef297df68fbbd6c0cf6f3
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-05-06 19:41:49 +0100

    Added data binding using asp-for for the SearchTerm property to keep the user entered search term and repopulate it

--- HomeToFood/Pages/Restaurants/List.cshtml.cs
+++ HomeToFood/Pages/Restaurants/List.cshtml.cs
@@ -18,18 +18,21 @@ namespace HomeToFood.Pages.Restaurants
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
 
+        //defaults on post. manually add get request on bind.
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
 
         public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
             this.restaurantData = restaurantData;
         }
-        public void OnGet(string searchTerm)
-        {
-            
 
+        public void OnGet()
+        {
             Message = config["Message"];
-            Restaurants = restaurantData.GetRestaurantsByName(searchTerm);
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
 }
