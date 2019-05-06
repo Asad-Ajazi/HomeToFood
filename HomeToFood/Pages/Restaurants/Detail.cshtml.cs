ec270dac5d41a4ff14bbaa39e99e9461d5072ac2
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-05-06 20:09:30 +0100

    Built a detail apge to display additional info about a restaurant. passes restaurantId from previous form, @page attribute modified to allow only /detail/id in url.

---
+++ HomeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeToFood.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomeToFood.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        public Restaurant Restaurant { get; set; }
+
+        public void OnGet(int restaurantId)
+        {
+            Restaurant = new Restaurant();
+            Restaurant.Id = restaurantId;
+        }
+    }
+}
