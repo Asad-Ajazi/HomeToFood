6667276cabb844c968185202ddcf38cc490f6af9
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-05-06 23:21:55 +0100

    Added an edit page to the list control. IEnumerable of select list item on PageModel to diplay the dropdown from CuisineType enum.

---
+++ HomeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeToFood.Core;
+using HomeToFood.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HomeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+                return RedirectToPage("./NotFound");
+
+            return Page();
+        }
+    }
+}
