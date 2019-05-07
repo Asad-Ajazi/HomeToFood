bd4af3a2afd3e3de9071dd8fe7c9bbdc14bf0f90
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-05-07 18:33:38 +0100

    Implmented SqlRestaurantData class to update database using entity framework core.

---
+++ HomeToFood.Data/SqlRestaurantData.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using HomeToFood.Core;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeToFood.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly HomeToFoodDbContext db;
+
+        public SqlRestaurantData(HomeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            db.Add(newRestaurant);
+            return newRestaurant;
+        }
+
+        // nothing will change in the database until commit is called.
+        public int Commit()
+        {
+            return db.SaveChanges();
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = GetById(id);
+            if (restaurant != null)
+            {
+                db.Restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return db.Restaurants.Find(id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            var query = from r in db.Restaurants
+                        where r.Name.StartsWith(name) || string.IsNullOrEmpty(name)
+                        orderby r.Name
+                        select r;
+            return query;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            //attach tracks changes to an object.
+            var entity = db.Restaurants.Attach(updatedRestaurant);
+            entity.State = EntityState.Modified;
+            return updatedRestaurant;
+        }
+    }
+
+}
