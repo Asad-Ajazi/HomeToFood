cec1bbbb13e3ff97e5e9ebddf8d7bb22e90dcb64
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-05-07 17:48:58 +0100

    Added entityframeworkCore and configured in app settings/startup.cs to use a DbContext.

---
+++ HomeToFood.Data/HomeToFoodDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HomeToFood.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeToFood.Data
+{
+    public class HomeToFoodDbContext : DbContext
+    {
+        public HomeToFoodDbContext()
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
